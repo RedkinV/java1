2580103d1402ea3e7d18454d8f49da4280648744
vvr <ab9171818@gmail.com>
2018-08-08 13:53:13 -0500

    добавил вывод в консоль и варианта №2(метод eat в классе Plate)

--- dz7_redkin_vladimir/Main.java
+++ dz7_redkin_vladimir/Main.java
@@ -11,15 +11,23 @@ public class Main {
         arrCat[4] = new Cat("Tishka", 17,false);
         Plate plate = new Plate(30,60);
 
-
+        System.out.println("---Вариант 1, метод eat в классе Cat----------------------------------------------");
         plate.info();
-
         for (int i = 0; i <arrCat.length ; i++) {
-            plate.addFood((i%2)*10);
+            plate.addFood((i%2)*10);//добавление еды
             arrCat[i].eat(plate);
-            System.out.printf("%-15S Сытость  %-15b Остаток еды %d %n",arrCat[i].getName(),arrCat[i].isFullness(),plate.getFood());
+            System.out.printf("%-20S Сытость - %-15b Остаток еды в тарелке - %d %n",arrCat[i].getName(),arrCat[i].isFullness(),plate.getFood());
         }
+        plate.info();
+        System.out.println("---Вариант 2, метод eat в классе Plate---------------------------------------------");
 
+        plate = new Plate(30,60);
+        plate.info();
+        for (int i = 0; i <arrCat.length ; i++) {
+            plate.addFood((i%2)*10);//добавление еды
+            plate.eat(arrCat[i]);
+            System.out.printf("%-20S Сытость - %-15b Остаток еды в тарелке - %d %n",arrCat[i].getName(),arrCat[i].isFullness(),plate.getFood());
+        }
         plate.info();
     }
 
